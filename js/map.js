08dcc15dbe8c6690d6b367b92d0db951569f0d10
John Mellaley <mellaley@hotmail.com>
2018-12-10 16:51:34 +0000

    search loaction

--- js/map.js
+++ js/map.js
@@ -8,11 +8,6 @@ function initMap() {
         center: { lat: 53.142, lng: 7.6921 },
         zoom: 4
         });
-        
-   
-         document.getElementById('submit').addEventListener('click', function() {
-         geocodeAddress(geocoder, map);
-             });
        
 } 
 
@@ -39,18 +34,12 @@ function codeAddress() {
     var address = document.getElementById('city').value;
     geocoder.geocode( { 'address': address}, function(results, status) {
         if (status === 'OK') {
-            
-            alert(results[0].geometry.location)
-            setCenter(results[0].geometry.location);
+         //   alert(results[0].geometry.location)
+            map.setCenter(results[0].geometry.location);
             var marker = new google.maps.Marker({
                 map: map,
                 position: results[0].geometry.location
                 });
-                map = new google.maps.Map(document.getElementById('map'), {
-                center: {test},
-                zoom: 4
-        });
-                
         
         } else {
             alert('Geocode was not successful for the following reason: ' + status);
