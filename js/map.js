fa36f0e490ee6b0af7f98d19ac588fcba14ef41c
John Mellaley <mellaley@hotmail.com>
2018-12-11 11:32:13 +0000

    updated code ot make clearer

--- js/map.js
+++ js/map.js
@@ -1,36 +1,23 @@
 var geocoder;
 var map;
-
+var i = 0;
 
 function initMap() {
+
+    if(i === 0 ) {
+        i++;
+        alert("hello1")
         geocoder = new google.maps.Geocoder();
         map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 53.142, lng: 7.6921 },
         zoom: 4
         });
+    }
        
 } 
 
-
-/*function geocodeAddress(geocoder, resultsMap) {
-  var address = document.getElementById('city').value;
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === 'OK') {
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-    } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}*/
-
-
-
 function codeAddress() {
-  
+    alert("helloo2");
     var address = document.getElementById('city').value;
     geocoder.geocode( { 'address': address}, function(results, status) {
         if (status === 'OK') {
